e6fc00db8fff77312a7b383f386951cafc0c1fef
MagneFire <IDaNLContact@gmail.com>
2021-01-07 16:34:33 +0100

    msm8226: Include kernel headers to path.

--- msm8226/common.mk
+++ msm8226/common.mk
@@ -59,3 +59,4 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
+kernel_includes += $(LOCAL_PATH)/../../../msm8x26/kernel-headers/
